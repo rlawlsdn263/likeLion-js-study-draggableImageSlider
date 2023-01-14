2ee0fc8ca92277ff8749ce05e4670df381f76419
rlawlsdn263 <rlawlsdn263@naver.com>
2023-01-14 21:30:38 +0900

    [설정] 태초마을

---
+++ main.js
@@ -0,0 +1,22 @@
+// const gallery = document.querySelector(".gallery");
+// const carousel = document.querySelector(".gallery-carousel");
+
+// let pressed = false;
+// let startX;
+// let x;
+
+// gallery.addEventListener("mouseenter", () => {
+//   gallery.style.cursor = "grab";
+// });
+
+// gallery.addEventListener("mousedown", (e) => {
+//   pressed = true;
+//   startX = e.offsetX - carousel.offsetLeft;
+//   gallery.style.cursor = "grabbing";
+//   console.log(e.offsetX, carousel.offsetLeft);
+// });
+
+// gallery.addEventListener("mouseup", () => {
+//   gallery.style.cursor = "grab";
+//   pressed = false;
+// });
